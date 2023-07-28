149ed511b952fb9e3e8eed3831868991611b590f
matt11matthew <matt11matthew@gmail.com>
2023-07-28 12:05:09 -0400

    FINISHED

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -6,69 +6,10 @@
 int extraMemoryAllocated;
 
 
+
+void mergeSort(int pData[], int l, int r);
 void swap(int *a, int *b);
-
-void merge(int arr[], int l, int m, int r) {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    /* create temp arrays */
-    int L[n1], R[n2];
-
-    extraMemoryAllocated += (n1 + n2) * sizeof(int);
-
-
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
-
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    /* Copy the remaining elements of L[], if there are any */
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    /* Copy the remaining elements of R[], if there are any */
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-// implement merge sort
-// extraMemoryAllocated counts bytes of extra memory allocated
-void mergeSort(int pData[], int l, int r)
-{
-    if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for large l and h
-        int m = l + (r - l) / 2;
-
-        // Sort first and second halves
-        mergeSort(pData, l, m);
-        mergeSort(pData, m + 1, r);
-
-        merge(pData, l, m, r);
-    }
-}
+void merge(int arr[], int l, int m, int r);
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n) {
@@ -84,7 +25,6 @@ void insertionSort(int* pData, int n) {
         pData[j + 1] = key;
     }
 }
-
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n) {
@@ -97,8 +37,6 @@ void bubbleSort(int* pData, int n) {
         }
     }
 }
-
-
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
@@ -119,6 +57,8 @@ void selectionSort(int* pData, int n) {
         }
     }
 }
+
+
 
 // parses input file to an integer array
 int parseData(char *inputFileName, int **ppData)
@@ -167,7 +107,8 @@ int main(void)
 	int i;
     double cpu_time_used;
 //	char* fileNames[] = {"input1.txt", "input2.txt", "input3.txt"};
-	char* fileNames[] = {"C:\\Users\\matt1\\CLionProjects\\compareSortingAlgorithms\\input1.txt", "C:\\Users\\matt1\\CLionProjects\\compareSortingAlgorithms\\input2.txt", "C:\\Users\\matt1\\CLionProjects\\compareSortingAlgorithms\\input3.txt"};
+//	char* fileNames[] = {"C:\\Users\\matt1\\CLionProjects\\compareSortingAlgorithms\\input1.txt", "C:\\Users\\matt1\\CLionProjects\\compareSortingAlgorithms\\input2.txt", "C:\\Users\\matt1\\CLionProjects\\compareSortingAlgorithms\\input3.txt"};
+	char* fileNames[] = {"C:\\Users\\Matthew Eisenberg\\Documents\\DREnhancedMod\\Projects\\DRMod\\compareSortingAlgorithms\\input1.txt", "C:\\Users\\Matthew Eisenberg\\Documents\\DREnhancedMod\\Projects\\DRMod\\compareSortingAlgorithms\\input2.txt", "C:\\Users\\Matthew Eisenberg\\Documents\\DREnhancedMod\\Projects\\DRMod\\compareSortingAlgorithms\\input3.txt"};
 
 	for (i=0;i<3;++i)
 	{
@@ -232,4 +173,68 @@ int main(void)
 		free(pDataSrc);
 	}
 
+}
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+//    int n1 = m - l + 1;
+    int n1 = m - l + 1;
+//    int n2 = r - m;
+     int n2 = r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    extraMemoryAllocated += (n1 + n2) * sizeof(int);
+
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+// implement merge sort
+// extraMemoryAllocated counts bytes of extra memory allocated
+void mergeSort(int pData[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for large l and h
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+
+        merge(pData, l, m, r);
+    }
 }
